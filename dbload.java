10fbf305b6d6e43fa302e5c55cf7bed227d4ffa5
Anthony Mai <s3550974@student.rmit.edu.au>
2018-03-24 15:57:36 +1100

    test csv added, java now reads test csv file with fixed directory, prints names of the businesses

--- dbload.java
+++ dbload.java
@@ -6,9 +6,35 @@ public class dbload{
 	static String datafile;
 
 	public static void main (String[] args){
+		BufferedReader br = null;
+		String csvDelimiter = "\t";
+		String dataFileDir = "C:/Users/Coffee/Documents/COSC2406-2407/";
+		String line;
+
 		if(args[0].equals("-p")){
 			pagesize = Integer.parseInt(args[1]);
 			datafile = args[2];
+		}
+
+		try{
+			br = new BufferedReader(new FileReader(dataFileDir + datafile));
+			while((line = br.readLine()) != null){
+				String[] test = line.split(csvDelimiter);
+				System.out.println("test: " + test[1]);
+
+			}
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if (br != null){
+				try{
+					br.close();
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
